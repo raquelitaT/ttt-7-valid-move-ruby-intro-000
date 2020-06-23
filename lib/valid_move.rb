332a154315f33e2631f655f76d9ee4ef7ae00664
Raquel Tessarollo <tessarollo.raquel@gmail.com>
2020-06-23 14:17:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,3 @@
 def valid_move?(board, index)
-  if board[index].between?(board[0], board[8])
-  else board[index] != " " && board[index] != "" && board[index] != nil
-  end
+  board[index].between?(board[0], board[8])
 end
